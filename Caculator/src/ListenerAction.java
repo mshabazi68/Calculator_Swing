5fec3fecd9f80a332eed20768be539f7eb50765e
Shah <Shah@172.31.98.117>
2019-08-04 03:07:25 -0700

    Add the mouse clicking

--- Caculator/src/ListenerAction.java
+++ Caculator/src/ListenerAction.java
@@ -1,4 +1,33 @@
+/*
+	@author Mohammadreza Shahbazi Jalali
+	Due date : 9/18/2018
+	Class name: Comp 585
+	Purpose : This Class will initiate the KeyListener which is for mouse click.
+ */
+import javax.swing.JOptionPane;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class ListenerAction {
+public class ListenerAction extends Component implements ActionListener {
+    private KeyListener keyListener;
+    //Objects
+    private FrameSwing swingCalculator = new FrameSwing();
+    private Calculation compute = new Calculation();
+    private GetString getString = new GetString();
+    //Variables
+    private boolean isNewEntry = true;
+    private boolean isInitialExecution = true;
+    private String operatorPressed;
 
+
+// method for mouse listener
+   
+
+
+ //Quick and dirty way to find double of the same operator
 }
+
+

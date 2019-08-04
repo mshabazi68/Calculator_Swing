a00809bf471449c29aa6fc41e9299ab0ba8ddf28
Shah <Shah@172.31.98.117>
2019-08-03 17:24:48 -0700

    adding the driver

--- Caculator/src/Driver.java
+++ Caculator/src/Driver.java
@@ -1,4 +1,12 @@
-
+/*
+	@author Mohammadreza Shahbazi Jalali
+	Class name: Comp 585
+	Purpose : Main Class that run the program.
+ */
 public class Driver {
+	public static void main (String [] args) {
+
+		new FrameSwing("Calculator");
+	}
 
 }
